22f4da25d723ec566d0c457c54094df7dfcef2fd
Aarav Patel <aaravpatel14.in@gmail.com>
2026-09-20 14:31:58 +0530

    Create 07_ultrasonic_ranger.ino

---
+++ src/07_ultrasonic_ranger/07_ultrasonic_ranger.ino
@@ -0,0 +1,25 @@
+// Measures distance using an HC-SR04 ultrasonic sensor
+const int TRIG_PIN = 9;
+const int ECHO_PIN = 10;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  float distanceCm = duration * 0.034 / 2; // Calculate distance based on sound speed
+
+  Serial.print("Distance: ");
+  Serial.print(distanceCm);
+  Serial.println(" cm");
+  delay(300);
+}
